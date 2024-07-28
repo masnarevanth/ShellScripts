55df36e72de2154bcfeff869f21d9cec1b813495
root <root@ip-10-1-1-163.ec2.internal>
2024-07-28 09:18:34 +0000

    commit

--- script2.sh
+++ script2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #script to convert mem status into %
 
-TOTAL=`free -m |awk '/mem/ {print $2}'`
-USED=`free -m |awk '/mem/ {print $3}'`
-FREE=`free -m |awk '/mem/ {print $4}'`
+TOTAL=`free -m |awk '/Mem/ {print $2}'`
+USED=`free -m |awk '/Mem/ {print $3}'`
+FREE=`free -m |awk '/Mem/ {print $4}'`
+
 echo "total mem = $TOTAL MB"
-echo "used mem = $(($USED*100/$TOTAL)) % "
-echo "free mem = $(($FREE*100/TOTAL)) % "
+echo "used mem = $(($USED*100/$TOTAL)) %"
+echo "free mem = $(($FREE*100/$TOTAL)) %"
